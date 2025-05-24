081a1b7379d15599c689178a8b311534557634d4
Jamesudesu <angelesjames956@gmail.com>
2025-05-24 11:41:53 +0800

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-com.mycompany.motorphemployeeapp;
+package com.mycompany.motorphemployeeapp;
 /**
  *
  * @author Mico Uy
